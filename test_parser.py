df04d3f886e3caccc5c57f177adf7e79def64cc9
Shubhankar Sinha <161740451+Shubhu02008@users.noreply.github.com>
2026-07-28 11:53:39 +0530

    Delete test_parser.py

--- test_parser.py
+++
@@ -1,9 +0,0 @@
-from parser import tokenize, evaluate_tokens
-
-
-def test_tokenize():
-    assert tokenize("5 + 10 * 3") == [5, "+", 10, "*", 3]
-
-
-def test_basic_bodmas():
-    assert evaluate_tokens(tokenize("5 + 10 * 3")) == 35
