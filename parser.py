8955706abf871e1e7eefaa0a41923c4c5488e545
Shubhankar Sinha <161740451+Shubhu02008@users.noreply.github.com>
2026-07-28 11:43:02 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,1 +1,20 @@
-tokenize("5 + 10 * 3")
+def tokenize(expression):
+    tokens = []
+    number = ""
+
+    for char in expression:
+        if char.isdigit() or char == ".":
+            number += char
+
+        else:
+            if number:
+                tokens.append(float(number) if "." in number else int(number))
+                number = ""
+
+            if char.strip():
+                tokens.append(char)
+
+    if number:
+        tokens.append(float(number) if "." in number else int(number))
+
+    return tokens
